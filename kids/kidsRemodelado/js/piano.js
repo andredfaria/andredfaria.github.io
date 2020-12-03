dc6eafd975d9598441090144fdfd24770ae31897
André <adfariacarvalho@gmail.com>
2020-12-02 21:46:55 -0300

    projeto remodelado

---
+++ kids/kidsRemodelado/js/piano.js
@@ -0,0 +1,28 @@
+function doo() {
+    var nota = document.getElementById("audio1");
+    nota.play();
+  }
+  function re() {
+    var nota = document.getElementById("audio2");
+    nota.play();
+  }
+  function mi() {
+    var nota = document.getElementById("audio3");
+    nota.play();
+  }
+  function fa() {
+    var nota = document.getElementById("audio4");
+    nota.play();
+  }
+  function sol() {
+    var nota = document.getElementById("audio5");
+    nota.play();
+  }
+  function la() {
+    var nota = document.getElementById("audio6");
+    nota.play();
+  }
+  function si() {
+    var nota = document.getElementById("audio7");
+    nota.play();
+  }
